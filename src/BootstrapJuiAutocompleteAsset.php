6e7e6dead381244cfed6f7256bd6400801698100
Denis Po <denis.pogrebnoy@gmail.com>
2017-11-24 12:42:36 +0300

    n/a

--- src/BootstrapJuiAutocompleteAsset.php
+++ src/BootstrapJuiAutocompleteAsset.php
@@ -5,7 +5,7 @@ namespace denis909\yii;
 class BootstrapJuiAutoCompleteAsset extends \yii\web\AssetBundle
 {
 
-	public $sourcePath = '@vendor/denis909/bootstrap-jui-autocomplete/src/assets';
+	public $sourcePath = '@vendor/denis909/yii2-bootstrap-jui-autocomplete/src/assets';
 
     public $css = [
         'bootstrap-jui-autocomplete.css'
